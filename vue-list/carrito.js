1d7abcc8ce5f54820040f9c6d51ee05a37dd39c8
ArturoLat <alatorca22@alumnes.ub.edu>
2023-05-12 12:26:38 +0200

    Carrito Finalitzat Style.css y Carrito.css Responsive

--- vue-list/carrito.js
+++ vue-list/carrito.js
@@ -1,39 +1,98 @@
 const datos = {
-
-    headers: ['Producto', 'Cantidad', 'Precio'],
+    headers: ["Producto", "Cantidad", "Precio", ""],
     productos: [
         {
-            Producto: 'Camisa blanca',
-            Cantidad: 2,
-            Precio: 20.0
+            nombre: 'Creatina Monohydrate Biotech',
+            cantidad: 2,
+            precio: 20.0,
+            imagen: './imagenes/ofertas-images/creatina.jpg'
         },
         {
-            Producto: 'Pantalón negro',
-            Cantidad: 1,
-            Precio: 35.0
+            nombre: 'Creatina Creapure',
+            cantidad: 1,
+            precio: 35.0,
+            imagen: './imagenes/slider-images/creatine-offer.jpg'
         },
     ]
-
 }
 
-//Construimos la tabla, asignando el header y despues el resto de filas
-const tableHeader = document.getElementById("table-header");
-const tableBody = document.getElementById("table-body");
+function eliminarFila(event) {
+    const fila = event.target.closest("tr");
+    const confirmacion = confirm('¿Está seguro de que desea eliminar esta fila?');
+    if (confirmacion) {
+        fila.remove();
+        calcularTotal();
+    }
+}
 
-const rowHeader = document.createElement("tr");
-datos.headers.forEach(header => {
-    const th = document.createElement("th");
-    th.textContent = header;
-    rowHeader.appendChild(th);
-});
-tableHeader.appendChild(rowHeader);
+function calcularTotal() {
+    let totalPrice = 0;
+    document.querySelectorAll('#table-body tr').forEach(row => {
+        const cantidad = parseInt(row.querySelector('input[type="number"]').value);
+        const precio = parseFloat(row.querySelector('input[type="number"]').dataset.precio);
+        totalPrice += cantidad * precio;
+    });
+
+    totalAmount.textContent = totalPrice.toFixed(2) + ' €';
+}
+
+const tableBody = document.getElementById("table-body");
+const totalAmount = document.getElementById("total-amount");
+
+let totalPrice = 0;
 
 datos.productos.forEach(producto => {
     const row = document.createElement("tr");
     row.innerHTML = `
-      <td>${producto.Producto}</td>
-      <td>${producto.Cantidad}</td>
-      <td>${producto.Precio}</td>
-  `;
+        <td>
+            <div class="d-flex align-items-center">
+                <img src="${producto.imagen}" alt="${producto.nombre}" style="width: 50px; height: 50px; object-fit: contain; margin-right: 10px;">
+                <span>${producto.nombre}</span>
+            </div>
+        </td>
+        <td>
+            <input type="number" min="0" value="${producto.cantidad}" data-precio="${producto.precio}">
+        </td>
+        <td>${producto.precio} €</td>
+        <td class="eliminar-column">
+            <button class="btn-eliminar" style="display: none;">Eliminar</button>
+        </td>
+    `;
     tableBody.appendChild(row);
+
+    const inputCantidad = row.querySelector('input[type="number"]');
+    const btnEliminar = row.querySelector('.btn-eliminar');
+
+    btnEliminar.addEventListener("click", eliminarFila);
+
+    inputCantidad.addEventListener('input', () => {
+        calcularTotal();
+    });
+
+    row.addEventListener('mouseover', () => {
+        btnEliminar.style.display = 'block';
+    });
+
+    // Ocultar el botón de eliminar al quitar el mouse de la fila
+    row.addEventListener('mouseout', () => {
+        btnEliminar.style.display = 'none';
+    });
+
+    totalPrice += producto.cantidad * producto.precio;
+});
+
+totalAmount.textContent = totalPrice.toFixed(2) + ' €';
+
+const btnFinalizarCompra = document.getElementById("btn-finalizar-compra");
+
+btnFinalizarCompra.addEventListener("click", () => {
+    if (confirm("¿Estás seguro de finalizar la compra?")) {
+        alert("Tu paquete está siendo enviado");
+
+        // Vaciar la tabla
+        tableBody.innerHTML = "";
+
+        // Calcular el total
+        calcularTotal();
+    }
 });
