46da2379f29a05fac44c16915bf0fc43a9de91fb
leuis123 <lluis.alcala98@gmail.com>
2023-05-18 17:33:57 +0200

    correccio bug

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
-import Vue from 'vue';
-import Router from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import calculadora from "../components/calculadora";
 import inici from '../components/inici';
 import carrito from "../components/carrito";
@@ -8,47 +7,50 @@ import perfil from "../components/perfil";
 import blog from "../components/blog"
 import objetivos from "../components/objetivos";
 
-Vue.use(Router)
+const routes = [
+  {
+    path: '/',
+    name: 'inici',
+    component: inici
+  },
+  {
+    path: '/calculadora',
+    name: 'calculadora',
+    component: calculadora
+  },
+  {
+    path: '/carrito',
+    name: 'carrito',
+    component: carrito
+  },
+  {
+    path: '/producte/:parametro',
+    name: 'producte',
+    component: producte,
+    props: true
+  },
+  {
+    path: '/perfil',
+    name: 'perfil',
+    component: perfil
+  },
+  {
+    path: '/blog/:parametro',
+    name: 'blog',
+    component: blog
+  },
+  {
+    path: '/objetivos',
+    name: 'objetivos',
+    component: objetivos
+  }
+];
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'inici',
-      component: inici
-    },
-    {
-      path: '/calculadora',
-      name: 'calculadora',
-      component: calculadora
-    },
-    {
-      path: '/carrito',
-      name: 'carrito',
-      component: carrito
-    },
-    {
-      path: '/producte/:parametro',
-      name: 'producte',
-      component: producte,
-      props: true
-    },
-    {
-      path: '/perfil',
-      name: 'perfil',
-      component: perfil
-    },
-    {
-      path: '/blog/:parametro',
-      name: 'blog',
-      component: blog
-    },
-    {
-      path: '/objetivos',
-      name: 'objetivos',
-      component: objetivos
-    }
-  ]
-})
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+});
+
+export default router;
 
 
